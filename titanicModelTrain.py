925a133ba68cd7300a926c40dbf509aaf0b76ae3
BharathC15 <bharathcb@yahoo.in>
2020-02-03 17:08:27 +0530

    Supress the warning

--- titanicModelTrain.py
+++ titanicModelTrain.py
@@ -29,7 +29,7 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 101)
 
 from sklearn.linear_model import LogisticRegression
-logmodel = LogisticRegression()
+logmodel = LogisticRegression(solver='liblinear')
 logmodel.fit(X_train,y_train)
 predictions = logmodel.predict(X_test)
 
