973a502c8c7aea26d7c618f7c795d3973a0f96d3
BharathC <bharathcb@yahoo.in>
2020-02-03 14:13:30 +0530

    Adding a simple titanic model for training

---
+++ titanicModelTrain.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+train=pd.read_csv('data/train.csv')
+
+def add_age(cols):
+    Age = cols[0]
+    Pclass = cols[1]
+    if pd.isnull(Age):
+        return int(train[train["Pclass"] == Pclass]["Age"].mean())
+    else:
+        return Age
+
+train["Age"] = train[["Age", "Pclass"]].apply(add_age,axis=1)
+train.drop("Cabin",inplace=True,axis=1)
+pd.get_dummies(train["Sex"])
+sex = pd.get_dummies(train["Sex"],drop_first=True)
+embarked = pd.get_dummies(train["Embarked"],drop_first=True)
+pclass = pd.get_dummies(train["Pclass"],drop_first=True)
+train = pd.concat([train,pclass,sex,embarked],axis=1)
+train.drop(["PassengerId","Pclass","Name","Sex","Ticket","Embarked"],axis=1,inplace=True)
+X = train.drop("Survived",axis=1)
+y = train["Survived"]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 101)
+
+from sklearn.linear_model import LogisticRegression
+logmodel = LogisticRegression()
+logmodel.fit(X_train,y_train)
+predictions = logmodel.predict(X_test)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, predictions))
